3c673e3ab2ff67a56bb754b9fd8a39bdfe06c685
aaronshivers <aaron@moonmail.org>
2019-09-03 15:10:46 -0500

    setup almost complete

--- index.js
+++
@@ -1,8 +0,0 @@
-const body = document.querySelector('body')
-const button = document.querySelector('button')
-
-button.addEventListener('click', () => {
-  const message = document.createElement('p')
-  message.textContent = 'Hi Everybody!'
-  body.appendChild(message)
-})
--- links.js
+++
@@ -1,8 +0,0 @@
-const body = document.querySelector('body')
-const links = document.querySelector('a')
-const button = document.querySelector('button')
-
-button.addEventListener('click', () => {
-  body.classList.toggle('blue')
-  links.classList.toggle('blue')
-})
